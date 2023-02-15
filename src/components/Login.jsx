7e2d5b2dccd10fa56e08508ac2e4960a4e7274e9
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-02-15 21:47:07 +0530

    lotgin and connect to spotify

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,39 +1,54 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 export default function Login() {
-
-    const handleClick = () =>
-    {
-       alert('The button is pressed!!') 
-    }
+    const handleClick =  () => {
+        const clientId = "992c58a1b20243659cbb022a7a22d210";
+        const redirectUrl = "http://localhost:3000/";
+        const apiUrl = "https://accounts.spotify.com/authorize";
+        const scope = [
+          "user-read-email",
+          "user-read-private",
+          "user-modify-playback-state",
+          "user-read-playback-state",
+          "user-read-currently-playing",
+          "user-read-recently-played",
+          "user-read-playback-position",
+          "user-top-read",
+        ];
+        window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope.join(
+          " "
+        )}&response_type=token&show_dialog=true`;
+      };
   return (
     <Container>
-       <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png" alt="spotify image" /> 
-       <button onClick={handleClick}>Connect Spotify</button>
+      <img
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"
+        alt="spotify image"
+      />
+      <button onClick={handleClick}>Connect Spotify</button>
     </Container>
-  )
+  );
 }
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column; 
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    width: 100vw;
-    background-color: #1db954;
-    gap: 5rem;
-    img{
-        height: 20vh;
-    }
-    button
-    {
-      padding: 1rem 5rem;
-      border-radius: 5rem;  
-      border: none;
-      color: white;
-      background-color: black;
-      font-size: 1.4rem;
-      cursor: pointer;
-    }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #1db954;
+  gap: 5rem;
+  img {
+    height: 20vh;
+  }
+  button {
+    padding: 1rem 5rem;
+    border-radius: 5rem;
+    border: none;
+    color: white;
+    background-color: black;
+    font-size: 1.4rem;
+    cursor: pointer;
+  }
 `;
