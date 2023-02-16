1c1ffd8a967579bcd1f8b6929c30a5fcbd5e6eca
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-02-16 09:52:27 +0530

    newChanges

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,38 +1,68 @@
 import axios from "axios";
-import {useStateProvider} from "../utils/StateProvider";
+import { useStateProvider } from "../utils/StateProvider";
 import React, { useEffect } from "react";
 import { reducerCases } from "../utils/Constants";
+import styled from "styled-components";
 
 export default function Playlist() {
-  const [{ token,playlists},dispatch] = useStateProvider();
+  const [{ token, playlists }, dispatch] = useStateProvider();
   useEffect(() => {
     const getPlayListData = async () => {
       const response = await axios.get(
-        "https://api.spotify.com/v1/me/playlists" , {
-            headers: {
-                Authorization: "Bearer "+token,
-            "Content-Type": "application/json"
-            }            
+        "https://api.spotify.com/v1/me/playlists",
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+            "Content-Type": "application/json",
+          },
         }
       );
-      const {items} = response.data;
-      const playlists = items.map(({name  , id})=>{
-        return {name , id};
+      const { items } = response.data;
+      const playlists = items.map(({ name, id }) => {
+        return { name, id };
       });
-      dispatch({type:reducerCases.SET_PLAYLISTS , playlists})
+      dispatch({ type: reducerCases.SET_PLAYLISTS, playlists });
     };
     getPlayListData();
   }, [token, dispatch]);
-  return <div>
-    <ul>
-        {
-            playlists.map(({name , id})=>
-            {
-                return (
-                    <li key={id}>{name}</li>
-                )
-            })
-        }
-    </ul>
-  </div>;
+
+
+  return (
+    <Container>
+      <ul>
+        {playlists.map(({ name, id }) => {
+          return <li key={id}>{name}</li>;
+        })}
+      </ul>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  color: #b3b3b3;
+  height: 100%;
+  overflow: hidden;
+  ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+    height: 50vh;
+    max-height: 100%;
+    overflow: auto;
+    &::-webkit-scrollbar {
+      width: 0.7rem;
+      &-thumb {
+        background-color: rgba(255, 255, 255, 0.6);
+      }
+    }
+    li {
+      transition: 0.3s ease-in-out;
+      cursor: pointer;
+      &:hover {
+        color: white;
+      }
+    }
+  }
+`;
