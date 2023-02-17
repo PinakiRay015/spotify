dae145a7a19641ce2585150413fe738200ab71bf
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-02-17 07:56:43 +0530

    newChanges

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 export default function Footer() {
 
-    const mystyle = {
-        backgroundColor: '#181818',
-        height: '100%',
-    }
   return (
-    <div style={mystyle}>Footer</div>
+    <Container>Footer</Container>
   )
 }
+
+const Container = styled.div`
+background-color: #181818;
+height: 100%;
+width: 100%;
+border-top: 1px solid #282828;
+display: grid;
+grid-template-columns: 1fr 2fr 1fr;
+align-items: center;
+justify-content: center;
+padding: 0rem 1rem;
+`;
